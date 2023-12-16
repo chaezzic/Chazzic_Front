1c023fceaf7a9e3e6d18e66da84382576d127d7c
NerdKang <kcjc6233@gmail.com>
2023-12-16 20:19:23 +0900

    Edit Output Page

--- src/pages/interview_output.js
+++ src/pages/interview_output.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Octokit } from "octokit";
 import Header from "../components/header.js";
 import '../style/AiOutput.css';
 
@@ -8,41 +7,53 @@ function Ai_output() {
   const [repositoryContent, setRepositoryContent] = useState([]);
 
   useEffect(() => {
-    // Fetch data from the Python server
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://http://13.48.130.241:5000/send_data_to_react');
-        const data = await response.json();
-
-        // Update state with fetched data
-        setRepositoryContent(data);
-        setIsLoading(false);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        setIsLoading(false);
-      }
-    };
-
-    // Call the fetchData function
-    fetchData();
+    const storedData = localStorage.getItem('responseFromServer');
+    if (storedData) {
+      const parsedData = JSON.parse(storedData);
+      setRepositoryContent(parsedData);
+      setIsLoading(false);
+    } else {
+      console.error('Error fetching data from local storage: Data not found');
+      setIsLoading(false);
+    }
   }, []); // Empty dependency array ensures that this effect runs once after the initial render
+
+  const filterTextAfterColon = (text) => {
+    const indexOfColon = text.indexOf(":");
+    return indexOfColon !== -1 ? text.substring(indexOfColon + 1).trim() : text;
+  };
 
   const renderContent = (content) => {
     // Render your content based on the fetched data
     // Modify this part based on the structure of your data
     return (
       <div>
-        {content.map((item, index) => (
-          <div key={index}>{/* Render each item in the content */}</div>
-        ))}
-      </div>
+      {content.map((item, index) => (
+        <div key={index} className="QuestionListFrame">
+          <div className="PortfolioNumber">포트폴리오 {index + 1}</div>
+          <div className="QuestionBox">
+            {item.map((question, subIndex) => (
+              <div key={subIndex} className="QuestionFrame">
+                <div className="QuestionNumber">질문 {subIndex + 1}</div>
+                <div className="QuestionTypo" key={subIndex}>{filterTextAfterColon(question)}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
     );
   };
 
   return (
     <div className="AiOutputBody">
       <Header />
-      {isLoading ? <p>Loading...</p> : renderContent(repositoryContent)}
+      <div className="OutputFrame">
+        <div className="OutputTitleBox">
+          <div style={{color: '#4285F4', fontSize: 18, fontFamily: 'Heebo', fontWeight: '700', wordWrap: 'break-word'}}>면접 예상 질문</div>
+        </div>
+        {isLoading ? <p>Loading...</p> : renderContent(repositoryContent)}
+      </div>
     </div>
   );
 }
